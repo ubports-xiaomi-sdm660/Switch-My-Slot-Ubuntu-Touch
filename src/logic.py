9946cc944e1c719ad998e3960fdf0a98df343c10
Shouko <shouko@airmail.cc>
2021-03-08 14:36:29 +0530

    Set prefix for bootctl

--- src/logic.py
+++ src/logic.py
@@ -1,15 +1,17 @@
 import pyotherside
 import os
 
-stream = os.popen('unset LD_PRELOAD; unset LD_LIBRARY_PATH; /system/bin/bootctl hal-info')
+prefix = 'unset LD_PRELOAD; unset LD_LIBRARY_PATH; /system/bin/bootctl '
+
+stream = os.popen(prefix + 'hal-info')
 output = stream.read()
 pyotherside.send('halVersion', output)
 
-stream = os.popen('unset LD_PRELOAD; unset LD_LIBRARY_PATH; /system/bin/bootctl get-number-slots')
+stream = os.popen(prefix + 'get-number-slots')
 output = stream.read()
 pyotherside.send('noOfSlots', output)
 
-stream = os.popen('unset LD_PRELOAD; unset LD_LIBRARY_PATH; /system/bin/bootctl get-current-slot')
+stream = os.popen(prefix + 'get-current-slot')
 output = stream.read()
 temp = output
 
@@ -19,12 +21,12 @@ if temp.__eq__(0):
 elif s1.__eq__(1):
     pyotherside.send('currentSlot', "B")
 
-stream = os.popen('unset LD_PRELOAD; unset LD_LIBRARY_PATH; /system/bin/bootctl get-suffix ' + temp)
+stream = os.popen(prefix + 'get-suffix ' + temp)
 output = stream.read()
 pyotherside.send('currentSlotSuffix', output)
 
 def switchSlotFunc():
     if temp.__eq__(0):
-        os.system("unset LD_PRELOAD; unset LD_LIBRARY_PATH; /system/bin/bootctl set-active-boot-slot " + "1 && sudo reboot")
+        os.system(prefix + 'set-active-boot-slot ' + '1 && sudo reboot') # will only reboot if no password is set on device
     elif temp.__eq__(1):
-        os.system("unset LD_PRELOAD; unset LD_LIBRARY_PATH; /system/bin/bootctl set-active-boot-slot " + "0 && sudo reboot")
+        os.system(prefix + 'set-active-boot-slot ' + '0 && sudo reboot')
